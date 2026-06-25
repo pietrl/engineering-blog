eb6ffb1db27b4e4e5a60d1c6123099cf494037bf
lptro <contact.lpietro@gmail.com>
2026-06-25 10:14:52 -0300

    chore: add Partytown and Sitemap Astro integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 
+import partytown from '@astrojs/partytown';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx(), partytown(), sitemap()]
 });
